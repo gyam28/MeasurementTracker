ad7714b707a61f2e83d2f48a3fd12c7ff3518e5f
Gina <fogasgeorgina@gmail.com>
2020-07-12 18:57:12 +0300

    Initial Commit

---
+++ MeasureTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeasureTracker
+//
+//  Created by Gina on 12.7.2020.
+//  Copyright © 2020 Gina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
